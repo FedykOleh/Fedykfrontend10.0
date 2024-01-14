247b4e0ac770c52aaec86c0a9392498e27d237cc
unknown <fedikoleg7@gmail.com>
2024-01-14 14:58:17 +0200

    homework3

---
+++ homework3/homework3.js
@@ -0,0 +1,7 @@
+
+let userInfoTwo = prompt('Enter your information ');
+let userInfoThree = prompt('Enter your information ');
+alert(`${ userInfoOne }${ userInfoTwo }${ userInfoThree }`);
+let userNumber = 8_5_6_3_2 ;
+userNumber = String(userNumber);
+console.log(`${userNumber[0]} ${userNumber[1]} ${userNumber[2]} ${userNumberl[3]} ${userNumber[4]} ` )

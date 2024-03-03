d152d48dfe7364347faa617c0303da153a29e726
unknown <fedikoleg7@gmail.com>
2024-03-03 13:53:38 +0200

    HW21

---
+++ homework21/homework21.js
@@ -0,0 +1,79 @@
+class Human{
+    constructor(options){
+        this.name = options.name
+        this.gender= options.gender
+    }
+}
+const mary = new Human({
+    name: 'mary',
+    gender :27,
+})
+
+const john = new Human({
+    name: 'john',
+    gender :70,
+})
+
+const ricardo = new Human({
+    name: 'ricardo',
+    gender :8,
+})
+const mike = new Human({
+    name: 'mike',
+    gender :23,
+})
+const lucy = new Human({
+    name: 'lucy',
+    gender :78,
+})
+const joe = new Human({
+    name: 'joe',
+    gender :100,
+})
+const lily = new Human({
+    name: 'lily',
+    gender :18,
+})
+
+class Flat{
+ arr = [] 
+ addInhabitant (inhabitant){
+    this.arr.push(inhabitant)
+ }
+ getArr(){
+    return this.arr
+ }
+
+}
+const flat = new Flat({})
+flat.addInhabitant(john) 
+flat.addInhabitant(mary) 
+const flat2 = new Flat({})
+flat2.addInhabitant(ricardo) 
+flat2.addInhabitant(mike) 
+const flat3 = new Flat({})
+flat3.addInhabitant(lucy) 
+flat3.addInhabitant(joe) 
+const flat4 = new Flat({})
+flat4.addInhabitant(lily) 
+
+class House{
+    constructor(options){
+        this.maxFlatCounter =options.maxFlatCounter
+    }
+flatArr =[]
+addFlat(flatName){
+    this.flatArr.push(flatName)
+    if(this.flatArr.length > this.maxFlatCounter){
+        console.log(`There is no empty flats `) }
+     
+    return this.flatArr 
+}
+}
+let house = new House({})
+house.maxFlatCounter = 3
+house.addFlat(flat)
+house.addFlat(flat2)
+house.addFlat(flat3)
+// house.addFlat(flat4)
+// console.log(house)

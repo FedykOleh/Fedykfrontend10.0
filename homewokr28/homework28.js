67c31c929713d6abdec925b360397f7c06fb54c1
unknown <fedikoleg7@gmail.com>
2024-03-10 18:43:46 +0200

    HW28

---
+++ homewokr28/homework28.js
@@ -0,0 +1,7 @@
+let pictures = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+
+let randomIndex = Math.floor(Math.random() * pictures.length);
+let randomPicture = pictures[randomIndex];
+
+document.write(randomPicture)
+
